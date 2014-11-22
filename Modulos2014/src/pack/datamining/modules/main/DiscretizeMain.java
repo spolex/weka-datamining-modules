90713b3bf2bfc56fe27af8a2e4db7a3a21beaae8
Alberto <Alberto@equipo-Alberto>
2014-11-22 16:37:21 +0100

    Modificado Main de Discretize

--- Modulos2014/src/pack/datamining/modules/main/DiscretizeMain.java
+++ Modulos2014/src/pack/datamining/modules/main/DiscretizeMain.java
@@ -16,23 +16,49 @@ public class DiscretizeMain {
 		
 		Instances instances = LoaderSaver.getMyLoader().loadArff(args[0]);
 		int intervalos = 10;
-		int pos;
+		int pos = instances.numAttributes() - 1;
 		
-		if (args[1] != null)
+		if (args.length > 1 && args.length < 4)
+		{ 
+			if (args[1] != null && args[2] != null)
+			{
+				if (args[1].equals("-I"))
+				{
+					System.out.println("I");
+					intervalos = Integer.valueOf(args[2]);
+				}
+				
+				if (args[1].equals("-C"))
+				{System.out.println("C");
+					pos = Integer.valueOf(args[2]);
+				}
+			}
+		}
+		if (args.length > 4 && args.length < 6)
 		{
-			intervalos = Integer.valueOf(args[1]);
+				if (args[1].equals("-I"))
+				{
+					intervalos = Integer.valueOf(args[2]);
+				}
+				
+				if (args[1].equals("-C"))
+				{
+					pos = Integer.valueOf(args[2]);
+				}
+				if (args[3] != null && args[4] != null)
+				{
+					if (args[3].equals("-I"))
+					{
+						intervalos = Integer.valueOf(args[4]);
+					}
+					if (args[3].equals("-C"))
+					{
+						pos = Integer.valueOf(args[4]);
+					}
+				}
 
 		}
 		
-		if (args[2] == null)
-		{
-			pos = instances.numAttributes()-1;
-		}
-		else
-		{
-			pos = Integer.valueOf(args[2]);
-		}
-
 		try 
 		{
 			Instances filteredInstances = Discretization.getDiscretized(instances,intervalos ,pos);
@@ -45,5 +71,4 @@ public class DiscretizeMain {
 	
 
 	}
-
-}
+	}
