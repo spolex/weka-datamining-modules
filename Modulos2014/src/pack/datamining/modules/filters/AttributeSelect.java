8430d19d65cddf2958cd496249249f4df801a43a
Alberto <Alberto@equipo-Alberto>
2014-11-22 12:01:39 +0100

    Ya funciona, aunque los resultados son distintos a Weka

--- Modulos2014/src/pack/datamining/modules/filters/AttributeSelect.java
+++ Modulos2014/src/pack/datamining/modules/filters/AttributeSelect.java
@@ -20,17 +20,22 @@ import weka.filters.supervised.attribute.AttributeSelection;
 public class AttributeSelect 
 {	
 	public static Instances getAttributeSelection(Instances originalInstances) throws Exception
-	{
-	//	originalInstances.setClassIndex(originalInstances.numAttributes()-1);
-		
+	{		
+		 J48 classJ48 = new J48();
+		 classJ48.setOptions(weka.core.Utils.splitOptions("-C 0.25 -M 2"));
 		 
+		 RandomForest classRF = new RandomForest();
+		 classRF.setOptions(weka.core.Utils.splitOptions("-I 10 -K 0 -S 1"));
+		 
+
 		 int numValuesClass = originalInstances.numClasses();
-		 Instances j48Inst = selectAttributes(originalInstances, new J48());
-		 Instances RFInst = selectAttributes(originalInstances, new RandomForest());
+		 Instances j48Inst = selectAttributes(originalInstances, classJ48);
+		 Instances RFInst = selectAttributes(originalInstances, classRF);
 		 
 		 double j48Fm, RFFm;
-		 
+		 System.out.println("j48"+ j48Inst.numAttributes());
 		 j48Fm = calculateFmeasure(j48Inst, numValuesClass);
+		 System.out.println("RF"+RFInst.numAttributes());
 		 RFFm = calculateFmeasure(RFInst, numValuesClass);
 		 
 		 if (j48Fm > RFFm)
@@ -46,30 +51,30 @@ public class AttributeSelect
 	}
 
 	private static Instances selectAttributes(Instances data, Classifier classifier) 
-	{
-				
+	{	
 		AttributeSelection filter = new AttributeSelection();
-		  ClassifierSubsetEval eval = new ClassifierSubsetEval();
-		  GreedyStepwise search = new GreedyStepwise();
-		  eval.setClassifier(classifier);
-		  search.setSearchBackwards(true);
-		  filter.setEvaluator(eval);
-		  filter.setSearch(search);
+		ClassifierSubsetEval eval = new ClassifierSubsetEval();
+		GreedyStepwise search = new GreedyStepwise();
+		try {
+			search.setOptions(weka.core.Utils.splitOptions("-T -1.7976931348623157E308 -N -1"));
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		eval.setClassifier(classifier);
+		search.setSearchBackwards(true);
+		filter.setEvaluator(eval);
+		filter.setSearch(search);
 		  try {
 			filter.setInputFormat(data);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		  // generate new data
 		  Instances newData = null;
 		try {
 			newData = Filter.useFilter(data, filter);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		  System.out.println(newData.numAttributes());
 		  
 		  return newData;
 	}
@@ -78,7 +83,7 @@ public class AttributeSelect
 			throws Exception {
 		 Classifier cls = new RandomForest();
 		 Evaluation eval = new Evaluation(originalInstances);
-		 Random rand = new Random(1);  // using seed = 1
+		 Random rand = new Random(1);  
 		 int folds = 10;
 		 eval.crossValidateModel(cls, originalInstances, folds, rand);
 		 return evaluateFmeasure(eval, numValuesClass);
@@ -90,8 +95,11 @@ public class AttributeSelect
 		double sum = 0.0;
 		
 		for (int i = 0 ; i < numValuesClass; i++)
+			{
 			sum += eval.fMeasure(i);
-		
+			System.out.println(eval.fMeasure(i)
+					);
+			}
 		
 		return sum;
 	}
